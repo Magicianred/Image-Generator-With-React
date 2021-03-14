ea300331f5281d5782303839019dd70d27e5da84
fynaghk <n.fidan_15@mail.ru>
2021-03-14 16:49:41 +0400

    React Project created!

---
+++ my-app/src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../header';
+import './style.css';
+
+class App extends Component {
+    render() { 
+        return ( 
+            <React.Fragment>
+              <Header/>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default App;
